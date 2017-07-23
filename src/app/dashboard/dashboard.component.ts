e282b4539509520a8174d9f2017d8376994c3122
Grace Feng <g.feng@sap.com>
2017-07-23 17:37:12 +0800

    import the Hero and HeroService to DashboardComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
