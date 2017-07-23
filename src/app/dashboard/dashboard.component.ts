b0ab91590362fb283475cd28e45078f841a7aa8a
Grace Feng <g.feng@sap.com>
2017-07-23 17:40:37 +0800

    Define a heroes array property & Inject the HeroService in the constructor and hold it in a private heroService field & Call the service to get heroes inside the Angular ngOnInit() lifecycle hook

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,15 +4,18 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+	heroes: Hero[] = [];
 
-  ngOnInit() {
-  }
+	constructor(private heroService: HeroService) { }
+
+	ngOnInit(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
+	}
 
 }
